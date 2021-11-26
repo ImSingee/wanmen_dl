58ce45b954f49b36476c65e09385334cf95d1d0f
Singee <git@singee.me>
2021-11-26 14:58:26 +0800

    允许配置是否启用多进程

--- download_course.py
+++ download_course.py
@@ -47,7 +47,10 @@ def fetch_course(course_id: str, course_name: str, base_dir: str, *, lecture_id=
 
 
 def fetch_all_chapters(chapters, base_dir: str):
-    fetch_all_chapters_with_multiprocessing(chapters, base_dir)
+    if CONFIG['MultiProcess']:
+        fetch_all_chapters_with_multiprocessing(chapters, base_dir)
+    else:
+        fetch_all_chapters_with_single_thread(chapters, base_dir)
 
 
 def fetch_all_chapters_with_single_thread(chapters, base_dir: str):
@@ -62,6 +65,7 @@ def fetch_all_chapters_with_multiprocessing(chapters, base_dir: str):
         for i, chapter in enumerate(chapters, 1):
             p.apply_async(fetch_chapter, (i, chapter, base_dir))
 
+        p.close()
         p.join()
 
 
