e33e88ee33a40ecc69e12200d899d4b86e86976c
Singee <git@singee.me>
2021-11-26 16:47:35 +0800

    进一步增强多线程

--- download_course.py
+++ download_course.py
@@ -41,7 +41,7 @@ def fetch_course(course_id: str, course_name: str, base_dir: str, *, lecture_id=
                 found = True
                 chapter_dir = os.path.join(base_dir, f"{i} - {chapter_name}")
                 os.makedirs(chapter_dir, exist_ok=True)
-                fetch_single(None, f'{i}-{j}', lecture, chapter_dir)
+                fetch_single(f'{i}-{j}', lecture, chapter_dir)
         if not found:
             print(f"lecture_id = {lecture_id} 的课程不存在")
             return
@@ -58,8 +58,18 @@ def fetch_all_chapters(chapters, base_dir: str):
 
 
 def fetch_all_chapters_with_single_thread(chapters, base_dir: str):
-    for i, chapter in enumerate(chapters, 1):
-        fetch_chapter(i, chapter, base_dir)
+    with Session() as session:
+        for chapter_index, chapter in enumerate(chapters, 1):
+            chapter_name = to_name(chapter['name'])
+
+            print(f"开始下载第 {chapter_index} 章：{chapter_name}")
+            chapter_dir = os.path.join(base_dir, f"{chapter_index} - {chapter_name}")
+            os.makedirs(chapter_dir, exist_ok=True)
+
+            for j, lecture in enumerate(chapter['children'], 1):
+                fetch_single(f'{chapter_index}-{j}', lecture, chapter_dir, session=session)
+
+            print(f"第 {chapter_index} 章：{chapter_name} 下载完成")
 
 
 def fetch_all_chapters_with_multiprocessing(chapters, base_dir: str):
@@ -68,31 +78,55 @@ def fetch_all_chapters_with_multiprocessing(chapters, base_dir: str):
 
     errors = []
     with Pool(CONFIG['NumProcess']) as p:
-        for i, chapter in enumerate(chapters, 1):
-            p.apply_async(fetch_chapter, (i, chapter, base_dir), error_callback=lambda e: errors.append(e))
+        for chapter_index, chapter in enumerate(chapters, 1):
+            chapter_name = to_name(chapter['name'])
+            chapter_dir = os.path.join(base_dir, f"{chapter_index} - {chapter_name}")
+            os.makedirs(chapter_dir, exist_ok=True)
+
+            # 预先跳过
+            tip = should_skip(f'{chapter_index}-{j}', lecture, chapter_dir)
+            if tip is not None:
+                print(tip)
+                continue
+
+            # 加入下载队列
+            for j, lecture in enumerate(chapter['children'], 1):
+                p.apply_async(fetch_single, (f'{chapter_index}-{j}', lecture, chapter_dir), error_callback=lambda err: errors.append(err))
 
         p.close()
         p.join()
 
     if len(errors) != 0:
+        for i, e in enumerate(errors, 1):
+            print(f"Error {i}: {e}")
+
         raise errors[0]
 
 
-def fetch_chapter(chapter_index: int, chapter: dict, base_dir: str):
-    chapter_name = to_name(chapter['name'])
+def ensure_session(f):
+    def inner(*args, **kwargs):
+        session = kwargs.get('session')
 
-    print(f"开始下载第 {chapter_index} 章：{chapter_name}")
-    chapter_dir = os.path.join(base_dir, f"{chapter_index} - {chapter_name}")
-    os.makedirs(chapter_dir, exist_ok=True)
+        if session is None:
+            with Session() as session:
+                kwargs['session'] = session
+                return f(*args, **kwargs)
+        else:
+            return f(*args, **kwargs)
 
-    with Session() as session:
-        for j, lecture in enumerate(chapter['children'], 1):
-            fetch_single(session, f'{chapter_index}-{j}', lecture, chapter_dir)
-
-    print(f"第 {chapter_index} 章：{chapter_name} 下载完成")
+    return inner
 
 
-def fetch_single(session, lecture_index: str, lecture_info: dict, base_dir: str):
+def should_skip(lecture_index: str, lecture_info: dict, base_dir: str):
+    lecture_name = lecture_index + ' ' + to_name(lecture_info['name'])
+    save_to = os.path.join(base_dir, lecture_name + '.mp4')
+
+    if os.path.exists(save_to):
+        return f"{save_to} 已存在 -> 跳过"
+
+
+@ensure_session
+def fetch_single(lecture_index: str, lecture_info: dict, base_dir: str, *, session):
     lecture_id = lecture_info['_id']
     lecture_name = lecture_index + ' ' + to_name(lecture_info['name'])
 
@@ -103,9 +137,6 @@ def fetch_single(session, lecture_index: str, lecture_info: dict, base_dir: str)
     if os.path.exists(save_to):
         print(save_to, "已存在 -> 跳过")
         return
-
-    if session is None:
-        session = requests
 
     r = session.get(f'https://api.wanmen.org/4.0/content/lectures/{lecture_id}?routeId=main&debug=1', headers=get_headers())
 
